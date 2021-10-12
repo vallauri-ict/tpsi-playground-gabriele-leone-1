56e966a291758299338d2d9eb389c58f12c822fb
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-10-12 09:13:12 +0200

    Esercizio 6
    Creato il servizio "PERSONE"

--- Node/Esercizio 6-  Persons/server.ts
+++ Node/Esercizio 6-  Persons/server.ts
@@ -4,13 +4,13 @@ import { json } from 'stream/consumers';
 import { resourceLimits } from 'worker_threads';
 let HEADERS = require("./headers.json");
 let dispatcher = require("./dispatcher.ts");
-let persons=require("./persons.json");
+let persons = require("./persons.json");
 
 let PORT: number = 1337;
 
 //creo il server
 let server = _http.createServer(function (req, res) {
-    dispatcher.dispatch(req,res);
+    dispatcher.dispatch(req, res);
 });
 
 //metto il server in ascolto sulla porta 1337
@@ -24,14 +24,33 @@ console.log("Server in ascolto sulla porta: " + PORT);
 //-------------------------
 
 dispatcher.addListener("GET", "/api/nazioni", function (req, res) {
-    res.writeHead(200, HEADERS.json);
-    let nazioni=[]//creo un vettore enumerativo 
+    let nazioni = []//creo un vettore enumerativo 
     for (const person of persons["results"]) {
-        if(!nazioni.includes(person.location.country)){
+        if (!nazioni.includes(person.location.country)) {
             nazioni.push(person.location.country)
         }
     }
     nazioni.sort() //ordinare alfabeticamente un vettore 
-    res.write(JSON.stringify({"nazioni":nazioni})); //rendo il vettore enumerativo un JSON
+    res.writeHead(200, HEADERS.json);
+    res.write(JSON.stringify({ "nazioni": nazioni })); //rendo il vettore enumerativo un JSON
     res.end(); //se c'è solo un write si può mettere il contenuto di write dentro end 
+})
+
+dispatcher.addListener("GET", "/api/persone", function (req, res) {
+    let nazione:string = req["GET"].nazione
+    let persone:object[]= []
+    for (const person of persons.results) {
+        if (person.location.country == nazione) {
+            let json={
+                "name":person.name.title + person.name.first +person.name.last ,
+                "city":person.location.city,
+                "state":person.location.state,
+                "cell":person.cell
+            }
+            persone.push(json)
+        }
+    }
+    res.writeHead(200,HEADERS.json)
+    res.write(JSON.stringify(persone))
+    res.end()
 })
--- Node/Esercizio 6-  Persons/static/index.js
+++ Node/Esercizio 6-  Persons/static/index.js
@@ -16,7 +16,32 @@ $(document).ready(function () {
         a.text(naz)
         a.val(naz)
         a.prop("id",naz)
+        a.on("click",visualizzaPersone)
         a.appendTo(_lstNazioni)
        }
     })
+
+    function visualizzaPersone(){
+        let param=$(this).text()
+        console.log(param)
+        let requestPersone=inviaRichiesta("GET","/api/persone",{"nazione":param}) //passarli in json così
+        requestPersone.fail(errore);
+        requestPersone.done(function(persons){
+            console.log(persons)
+            _tabStudenti.empty()
+            for (const person of persons) {
+                let tr=$("<tr>").appendTo(_tabStudenti)
+                for (const key in person) {
+                    $("<td>").appendTo(tr).html(person[key]) //noi dobbiamo cercare la chiave che ha valore il contenuto di key non posso fare .key
+                }
+                let td=$("<td>").appendTo(tr)
+                $("<button>").appendTo(td).html("dettagli")
+                td=$("<td>").appendTo(tr)
+                $("<button>").appendTo(td).html("elimina")
+            }
+        })
+    }
+
+    
+
 })
