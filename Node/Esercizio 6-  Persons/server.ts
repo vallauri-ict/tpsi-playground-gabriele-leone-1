6fa50996cea2bb571e6af1c08b6dcaabb21ac15c
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-10-13 09:32:40 +0200

    Esercizio 6
    Almost finished only navigation button management missing

--- Node/Esercizio 6-  Persons/server.ts
+++ Node/Esercizio 6-  Persons/server.ts
@@ -37,20 +37,66 @@ dispatcher.addListener("GET", "/api/nazioni", function (req, res) {
 })
 
 dispatcher.addListener("GET", "/api/persone", function (req, res) {
-    let nazione:string = req["GET"].nazione
-    let persone:object[]= []
+    let nazione: string = req["GET"].nazione
+    let persone: object[] = []
     for (const person of persons.results) {
         if (person.location.country == nazione) {
-            let json={
-                "name":person.name.title + person.name.first +person.name.last ,
-                "city":person.location.city,
-                "state":person.location.state,
-                "cell":person.cell
+            let json = {
+                "name": person.name.title + " " + person.name.first + " " + person.name.last,
+                "city": person.location.city,
+                "state": person.location.state,
+                "cell": person.cell
             }
             persone.push(json)
         }
     }
-    res.writeHead(200,HEADERS.json)
+    res.writeHead(200, HEADERS.json)
     res.write(JSON.stringify(persone))
     res.end()
+})
+dispatcher.addListener("PATCH", "/api/dettagli", function (req, res) {
+    let personReq = req["BODY"].person
+    let trovato: boolean = false
+    let person;
+    for ( person of persons.results) {
+        if ((person.name.title + " " + person.name.first + " " + person.name.last) == personReq) {
+            trovato = true;
+            break;
+        }
+    }
+    if (trovato) {
+        res.writeHead(200, HEADERS.json);
+        res.write(JSON.stringify(person))
+        res.end();
+    }
+    else
+    {
+        res.writeHead(404, HEADERS.json);
+        res.write("Persona non trovata")
+        res.end();
+    }
+})
+
+dispatcher.addListener("DELETE", "/api/elimina", function (req, res) {
+    let personReq = req["BODY"].person
+    let trovato: boolean = false
+    let i;
+    for ( i=0;i< persons.results.length;i++) {
+        if ((persons.results[i].name.title + " " + persons.results[i].name.first + " " + persons.results[i].name.last) == personReq) {
+            trovato = true;
+            break;
+        }
+    }
+    if (trovato) {
+        persons.results.splice(i,1)
+        res.writeHead(200, HEADERS.json);
+        res.write(JSON.stringify("Persona eliminata Correttamente")) //nel 200 ci vuole un JSON SERIALIZZATO
+        res.end();
+    }
+    else
+    {
+        res.writeHead(404, HEADERS.text);
+        res.write("Record Non trovato")
+        res.end();
+    }
 })
--- Node/Esercizio 6-  Persons/static/index.js
+++ Node/Esercizio 6-  Persons/static/index.js
@@ -3,45 +3,80 @@ $(document).ready(function () {
     let _lstNazioni = $("#lstNazioni");
     let _tabStudenti = $("#tabStudenti");
     let _divDettagli = $("#divDettagli");
-
+    let selectedNation
     _divDettagli.hide()
     let requestNazioni = inviaRichiesta("GET", "/api/nazioni") //tipo promise 
     requestNazioni.fail(errore);
     requestNazioni.done(function (data) {
         console.log(data)
-       for (const naz of data.nazioni) {
-        let a=$("<a>")
-        a.prop("class","dropdown-item")
-        a.prop("href","#")
-        a.text(naz)
-        a.val(naz)
-        a.prop("id",naz)
-        a.on("click",visualizzaPersone)
-        a.appendTo(_lstNazioni)
-       }
+        for (const naz of data.nazioni) {
+            let a = $("<a>")
+            a.prop("class", "dropdown-item")
+            a.prop("href", "#")
+            a.text(naz)
+            a.val(naz)
+            a.prop("id", naz)
+            a.on("click", visualizzaPersone)
+            a.appendTo(_lstNazioni)
+        }
     })
 
-    function visualizzaPersone(){
-        let param=$(this).text()
-        console.log(param)
-        let requestPersone=inviaRichiesta("GET","/api/persone",{"nazione":param}) //passarli in json così
+    function visualizzaPersone() {
+        if($(this).text()){
+            selectedNation=$(this).text()
+        }
+        let requestPersone = inviaRichiesta("GET", "/api/persone", { "nazione": selectedNation }) //passarli in json così
         requestPersone.fail(errore);
-        requestPersone.done(function(persons){
+        requestPersone.done(function (persons) {
             console.log(persons)
             _tabStudenti.empty()
             for (const person of persons) {
-                let tr=$("<tr>").appendTo(_tabStudenti)
+                let tr = $("<tr>").appendTo(_tabStudenti)
                 for (const key in person) {
                     $("<td>").appendTo(tr).html(person[key]) //noi dobbiamo cercare la chiave che ha valore il contenuto di key non posso fare .key
                 }
-                let td=$("<td>").appendTo(tr)
-                $("<button>").appendTo(td).html("dettagli")
-                td=$("<td>").appendTo(tr)
-                $("<button>").appendTo(td).html("elimina")
+                let td = $("<td>").appendTo(tr)
+                $("<button>").appendTo(td).html("dettagli").on("click", dettagli).prop("name", person.name) //normale
+                td = $("<td>").appendTo(tr)
+                $("<button>").appendTo(td).html("elimina").addClass("elimina").prop("name", person.name) //con selettori
             }
         })
     }
+    //delegated events allegare l'evento al genitore non al figlio
+    //questo pseudoselettore tutti i button che contengono nel loro testo elimina :
+    //_tabStudenti.on("click","button:contains(elimina)",function(){}
+    //invece questo selettore punta ai button che implementano la classe elimina:
+    _tabStudenti.on("click", ".elimina", elimina)
 
-    
+    function dettagli() {
+        let requestDettagli = inviaRichiesta("PATCH", "/api/dettagli", {
+            "person": $(this).prop("name")
+        });
+        requestDettagli.fail(errore)
+        let name = $(this).prop("name")
+        requestDettagli.done(function (person) {
+            console.log(person)
+            _divDettagli.show(1000)
+            $(".card-img-top").prop("src", person.picture.thumbnail)
+            $(".card-title").html(name)
+            let s = "<b>gender:</b> " + person.gender + "</br>"
+            s += "<b> email:</b>" + JSON.stringify(person.email) + "</br>"
+            s += "<b> address:</b>" + JSON.stringify(person.location) + "</br>"
+            s += "<b> dob:</b>" + JSON.stringify(person.dob) + "</br>"
+            $(".card-text").html(s)
+        })
+        
+    }
 
+    function elimina() {
+        let name = $(this).prop("name")
+        let requestElimina = inviaRichiesta("DELETE", "/api/elimina", { person: name })
+        requestElimina.fail(errore)
+        requestElimina.done(function(data){
+            alert(data)
+            visualizzaPersone()
+        })
+    }
+
+    //GESTIONE PULSANTI DA FARE
 })
