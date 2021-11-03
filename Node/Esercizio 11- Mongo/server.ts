ccafceab59205515d55f7f4a1cb7eda28a6ca8a5
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-11-03 08:04:39 +0100

    Update and Delete Exercise 11

--- Node/Esercizio 11- Mongo/server.ts
+++ Node/Esercizio 11- Mongo/server.ts
@@ -6,6 +6,8 @@ const port: number = 1337;
 const mongoClient = _mongodb.MongoClient;
 const dispatcher: Dispatcher = new Dispatcher();
 
+const CONNECTIONSTRING = "mongodb://127.0.0.1:27017"
+
 // tutte le volte che arriva una richgiesta dal client parte questa funzione
 const server = _http.createServer(function (req, res) {
     dispatcher.dispatch(req, res);
@@ -57,3 +59,40 @@ mongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
         console.log("errore nella connessione al databasse :" + err.message)
     }
 });
+// update
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+    if (!err) {
+        let db = client.db("5B_Studenti")
+        let collection = db.collection("Studenti")
+        collection.updateOne({"Nome":"Mario"}, {$set:{"Residenza":"Fossano"}}, function (err, data) {
+            if (!err) {
+                console.log("FIND",data)
+            } else {
+                console.log("Errore esecuzione query " + err.message)
+            }
+            client.close()
+        }) // updateOne(filtro, azione)
+            
+    } else {
+        console.log("Errore connessione al db")
+    }
+})
+
+// deleteMany
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+    if (!err) {
+        let db = client.db("5B_Studenti")
+        let collection = db.collection("Studenti")
+        collection.deleteMany({"Residenza":"Fossano"}, function (err, data) {
+            if (!err) {
+                console.log("DELETE",data)
+            } else {
+                console.log("Errore esecuzione query " + err.message)
+            }
+            client.close()
+        }) // updateOne(filtro, azione)
+            
+    } else {
+        console.log("Errore connessione al db")
+    }
+})
