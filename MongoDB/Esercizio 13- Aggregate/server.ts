acc92eebd773b46a388ba0cf057108953731ba98
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2021-11-16 08:50:51 +0100

    Finished Query 3,4,5

--- MongoDB/Esercizio 13- Aggregate/server.ts
+++ MongoDB/Esercizio 13- Aggregate/server.ts
@@ -16,27 +16,29 @@ const server = _http.createServer(function (req, res) {
 server.listen(port);
 console.log("Server in ascolto sulla porta " + port);
 
-//usiamo promise 
+//usiamo promise
 
-//QUERY 1- Si vogliono selezionare i record che hanno status A, raggrupparli secondo il campo cust_id e, 
+//QUERY 1- Si vogliono selezionare i record che hanno status A, raggrupparli secondo il campo cust_id e,
 //per ogni gruppo, eseguire la somma del campo amount (importo unitario)
 mongoClient.connect(CONNECTIONSTRING, function (err, client) {
   if (!err) {
     let db = client.db(DBNAME);
     let collection = db.collection("orders");
     //aggregate si aspetta un vettore enumerativo
-    //nel match posso utilizzare i filtri di find 
+    //nel match posso utilizzare i filtri di find
     //_id è una parola fissa che indica il campo su cui vogliamo fare i gruppi
-    //nell'id devo mettere il $ davanti al nome del campo 
+    //nell'id devo mettere il $ davanti al nome del campo
     //per ogni gruppo devo creare un campo che in questo caso è "totale"
     //nel "totale" dobbiamo mettere il nome del campo di cui vogliamo eseguire l'operazione e deve essere sempre preceduto dal dollaro
-    //a sinistra dei due punti no dollaro, a destra si 
-    //dopo $group il record set risultante avrà solo due colonne che sono quelle selezionate nel group 
-    let rq = collection.aggregate([
-      {"$match":{"status":"A"}},
-      {"$group":{"_id":"$cust_id","totale":{"$sum":"$amount"}}},
-      {"$sort":{"totale":-1}}
-    ]).toArray();
+    //a sinistra dei due punti no dollaro, a destra si
+    //dopo $group il record set risultante avrà solo due colonne che sono quelle selezionate nel group
+    let rq = collection
+      .aggregate([
+        { $match: { status: "A" } },
+        { $group: { _id: "$cust_id", totale: { $sum: "$amount" } } },
+        { $sort: { totale: -1 } },
+      ])
+      .toArray();
     rq.then(function (data) {
       console.log("Query 1: ", data);
     });
@@ -56,15 +58,17 @@ mongoClient.connect(CONNECTIONSTRING, function (err, client) {
   if (!err) {
     let db = client.db(DBNAME);
     let collection = db.collection("orders");
-    let rq = collection.aggregate([
-     {
-       "$group":{
-         "_id":"$cust_id",
-         "avgAmount":{"$avg":"$amount"},
-         "avgTotal":{"$avg":{"$multiply":["$qta","$amount"]}}
-       }
-     }
-    ]).toArray();
+    let rq = collection
+      .aggregate([
+        {
+          $group: {
+            _id: "$cust_id",
+            avgAmount: { $avg: "$amount" },
+            avgTotal: { $avg: { $multiply: ["$qta", "$amount"] } },
+          },
+        },
+      ])
+      .toArray();
     rq.then(function (data) {
       console.log("Query 2: ", data);
     });
@@ -78,3 +82,91 @@ mongoClient.connect(CONNECTIONSTRING, function (err, client) {
     console.log("errore nella connessione al databasse :" + err.message);
   }
 });
+
+//QUERY 3- Conteggio degli unicorni maschi e degli unicorni femmina
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+  if (!err) {
+    let db = client.db(DBNAME);
+    let collection = db.collection("unicorns");
+    let rq = collection
+      .aggregate([
+        { $match: { gender: { $exists: true } } },
+        { $group: { _id: "$gender", totale: { $sum: 1 } } },
+      ])
+      .toArray();
+    rq.then(function (data) {
+      console.log("Query 3: ", data);
+    });
+    rq.catch(function (err) {
+      console.log("Errore esecuzione query: " + err.message);
+    });
+    rq.finally(function () {
+      client.close();
+    });
+  } else {
+    console.log("errore nella connessione al databasse :" + err.message);
+  }
+});
+
+//QUERY 4-Calcolare il numero medio di vampiri uccisi dagli unicorni femmina e dagli unicorni maschi
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+  if (!err) {
+    let db = client.db(DBNAME);
+    let collection = db.collection("unicorns");
+    let rq = collection
+      .aggregate([
+        { $match: { gender: { $exists: true } } },
+        {
+          $group: {
+            _id: { gender: "$gender" },
+            mediaVampiri: { $avg: "$vampires" },
+          },
+        },
+      ])
+      .toArray();
+    rq.then(function (data) {
+      console.log("Query 4: ", data);
+    });
+    rq.catch(function (err) {
+      console.log("Errore esecuzione query: " + err.message);
+    });
+    rq.finally(function () {
+      client.close();
+    });
+  } else {
+    console.log("errore nella connessione al databasse :" + err.message);
+  }
+});
+
+//QUERY 5- : raggruppare gli unicorni per genere e tipo di pelo
+mongoClient.connect(CONNECTIONSTRING, function (err, client) {
+  if (!err) {
+    let db = client.db(DBNAME);
+    let collection = db.collection("unicorns");
+    let rq = collection
+      .aggregate([
+        { $match: { gender: { $exists: true } } },
+        {
+          $group: {
+            _id: { gender: "$gender", hair: "$hair" },
+            nEsemplari: { $sum: 1 },
+          },
+        },
+        { $sort: { nEsemplari: -1, _id: -1 } },
+      ])
+      .toArray();
+    rq.then(function (data) {
+      console.log("Query 5: ", data);
+    });
+    rq.catch(function (err) {
+      console.log("Errore esecuzione query: " + err.message);
+    });
+    rq.finally(function () {
+      client.close();
+    });
+  } else {
+    console.log("errore nella connessione al databasse :" + err.message);
+  }
+});
+
+//QUERY 6- numero medio di vampiri uccisi dagli unicorni complessivamente presenti nella collezione
