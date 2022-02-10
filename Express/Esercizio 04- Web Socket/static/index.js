18740844b5a0f9087fae6e195a6ed42d2755070f
Gabriele <62541822+gabriele-leone@users.noreply.github.com>
2022-02-10 10:27:38 +0100

    Exercise 04 button handling done

--- Express/Esercizio 04- Web Socket/static/index.js
+++ Express/Esercizio 04- Web Socket/static/index.js
@@ -1,77 +1,99 @@
-$(document).ready(function() {
-    let user={"username":"","room":""};  
-	// mi connetto al server che mi ha inviato la pagina,
-	// il quale mi restituisce il suo serverSocket
-	// io.connect é SINCRONO, bloccante
+$(document).ready(function () {
+    let serverSocket;
+    let btnConnetti = $("#btnConnetti");
+    let btnDisconnetti = $("#btnDisconnetti")
+    let btnInvia = $("#btnInvia");
 
-    $("#btnConnetti").on("click",function(){
-        
-    })
 
-	let serverSocket = io({transports:['websocket'], upgrade: false}).connect(); //così non perdo mai la connessione
+    handleBtn(true);
 
-    /* 1a) lo username viene inviato SOLO a connessione avvenuta
-	       in questo modo si evita di connetere/disconnettere + volte */
-	serverSocket.on('connect', function() {
-		console.log("connessione ok");
-        impostaUser();
-        serverSocket.emit("login",JSON.stringify(user)); //passiamo un messaggio di login al server dove comunichiamo l'username e la room a cui connettersi alla function di callback
+    let user = { username: "", room: "" };
+    // mi connetto al server che mi ha inviato la pagina,
+    // il quale mi restituisce il suo serverSocket
+    // io.connect é SINCRONO, bloccante
+    btnConnetti.on("click", function () { //la riga on fa semplicemente un associazione, se l'oggetto è undefined va in errore
+
+        serverSocket = io({
+            transports: ["websocket"],
+            upgrade: false,
+        }).connect(); //così non perdo mai la connessione
+        /* 1a) lo username viene inviato SOLO a connessione avvenuta
+               in questo modo si evita di connetere/disconnettere + volte */
+        serverSocket.on("connect", function () {
+            console.log("connessione ok");
+            impostaUser();
+            serverSocket.emit("login", JSON.stringify(user)); //passiamo un messaggio di login al server dove comunichiamo l'username e la room a cui connettersi alla function di callback
+        });
+
+        // 1b) utente valido / non valido
+        serverSocket.on("loginAck", function (data) {
+            if (data == "NOK") {
+                alert("Nome già esistente. Scegliere un altro nome");
+                impostaUser();
+                serverSocket.emit("login", JSON.stringify(user));
+            } else document.title = user.username;
+        });
+
+        // 2b) ricezione della risposta
+        serverSocket.on("message_notify", function (data) {
+            // ricezione di un messaggio dal server
+            data = JSON.parse(data);
+            visualizza(data.from, data.message, data.date);
+        });
+
+        serverSocket.on("disconnect", function () {
+            alert("Sei stato disconnesso!");
+            handleBtn(true);
+        });
+
+       handleBtn(false);
     });
 
-    // 1b) utente valido / non valido
-    serverSocket.on('loginAck', function(data) {
-		if (data=="NOK"){
-			alert("Nome già esistente. Scegliere un altro nome")
-            impostaUser();
-			serverSocket.emit("login", JSON.stringify(user));
-		}
-		else
-			document.title = user.username;
-    });  
 
-	// 2a) invio messaggio
-    $("#btnInvia").click(function() {
+    // 2a) invio messaggio
+    btnInvia.click(function () {
         let msg = $("#txtMessage").val();
         serverSocket.emit("message", msg);
+        $("#txtMessage").val("");
     });
-	
-    // 2b) ricezione della risposta
-    serverSocket.on('message_notify', function(data) {
-        // ricezione di un messaggio dal server			
-        data = JSON.parse(data);
-        visualizza(data.from, data.message, data.date); 
-    });  
 
-	// 3) disconnessione
-    $("#btnDisconnetti").click(function() {
+
+
+    // 3) disconnessione
+    btnDisconnetti.click(function () {
         serverSocket.disconnect();
+
     });
 
-    serverSocket.on('disconnect', function() {
-        alert("Sei stato disconnesso!");
-    });
+    function handleBtn(x) {
+        btnConnetti.prop("disabled", !x)
+        btnInvia.prop("disabled", x)
+        btnDisconnetti.prop("disabled", x)
+    }
 
-    function impostaUser(){
-        user.username = prompt("Inserisci lo username:"); //richiedo l'username 
-        if(user.username=="pippo" || user.username=="pluto")
-        {
-            user.room="room1";
-        }
-        else
-        {
-            user.room="defaultRoom"; //stanza standard di entrata
+    function impostaUser() {
+        user.username = prompt("Inserisci lo username:"); //richiedo l'username
+        if (user.username == "pippo" || user.username == "pluto") {
+            user.room = "room1";
+        } else {
+            user.room = "defaultRoom"; //stanza standard di entrata
         }
     }
 
     function visualizza(username, message, date) {
-        let wrapper = $("#wrapper")
+        let wrapper = $("#wrapper");
         let container = $("<div class='message-container'></div>");
         container.appendTo(wrapper);
 
         // username e date
-        date = new Date(date); //trasformo date da stringa a object 
-        let mittente = $("<small class='message-from'>" + username + " @" 
-		                  + date.toLocaleTimeString() + "</small>");
+        date = new Date(date); //trasformo date da stringa a object
+        let mittente = $(
+            "<small class='message-from'>" +
+            username +
+            " @" +
+            date.toLocaleTimeString() +
+            "</small>"
+        );
         mittente.appendTo(container);
 
         // messaggio
@@ -80,10 +102,10 @@ $(document).ready(function() {
 
         // auto-scroll dei messaggi
         /* la proprietà html scrollHeight rappresenta l'altezza di wrapper oppure
-           l'altezza del testo interno qualora questo ecceda l'altezza di wrapper
-		*/
+               l'altezza del testo interno qualora questo ecceda l'altezza di wrapper
+            */
         let h = wrapper.prop("scrollHeight");
         // fa scorrere il testo verso l'alto in 500ms
-        wrapper.animate({ "scrollTop": h }, 500);
+        wrapper.animate({ scrollTop: h }, 500);
     }
 });
